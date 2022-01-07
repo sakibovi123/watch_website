8c86e47954c8cc4c2712733f4a3f8c6e5039f8df
sakib <sakib@Sakib.sakib>
2022-01-07 21:06:08 +0600

    7 jan

--- App/models.py
+++ App/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime, date
 
-# Create your models here.
+
+class MoreImage(models.Model):
+    images = models.ImageField(upload_to="images/")
+
+
+class Size(models.Model):
+    size_title = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ["-id"]
+    
+    def __str__(self):
+        return self.size_title
+
+
+class ColorModel(models.Model):
+    color_title = models.CharField(max_length=255)
+    color_img = models.ImageField(upload_to="images/", null=True, blank=True)
+
+    def __str__(self):
+        return self.color_title
+
+
+class WatchModel(models.Model):
+    watch_name = models.CharField(max_field=255)
+    watch_image = models.ImageField(upload_to="images/")
+    more_images = models.ManyToManyField(MoreImage)
+    size = models.ForeignKey(Size, on_delete=models.CASCADE)
